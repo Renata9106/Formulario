b240d0b1f16a75685b90f7f3bc7d7903e99eab8c
Renata9106 <renatamartins631@gmail.com>
2021-06-17 15:44:42 -0300

    código final teste

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
-var app = angular.module('app', ['ngRoute']);
+function setData (){
+  let entradaName = document.getElementById('nome')
+  let entradaDuracao = document.getElementById('duracao')
+  let entradaInicio = document.getElementById('data-inicio')
+  let entradaFim = document.getElementById('data-fim')
+  let entradaDescricao = document.getElementById('descricao')
 
-app.config(function($routeProvider){
-    routeProvider
-          .div("/Nome" , {
-            templateUrl: "template/home.html"
 
-          })
-         .when("/Data de Início" , {
-          templateUrl: "template/p1.html"
-          
-          })
-          .when("/Data de Termino" , {
-            templateUrl: "template/pagina2.html"
-            
-          })
-          .when("/Duracao (Semanas)" , {
-            templateUrl: "template/p3.html"
-            
-          })
 
-          .otherwise({ redirectTo: "/" })
+  let name = entradaName.value
+  let duracao = entradaDuracao.value
+  let inicio = entradaInicio.value
+  let fim = entradaFim.value
+  let descricao = entradaDescricao.value
 
-})
 
-function cadastraProduto(){
-  var pro
+  let tabela = document.getElementById("dadosTabela")
+
+  let dataUser = [
+    {name, inicio, fim, duracao, descricao },
+   
+  ]
+
+  localStorage.setItem("dataUser", JSON.stringify(dataUser))
+
+  let infoUser = JSON.parse(localStorage.getItem("dataUser"))
+
+ for (let i = 0; i < infoUser.length; i++) {
+  tabela.innerHTML += "<tr><td>" + infoUser[i].name + "</td>"
+    + "<td>" + infoUser[i].inicio.replace(/(\d*)-(\d*)-(\d*).*/, '$3-$2-$1') + "</td>" 
+    + "</td><td>" + infoUser[i].fim.replace(/(\d*)-(\d*)-(\d*).*/, '$3-$2-$1')  + "</td><td>"
+    + infoUser[i].duracao + "</td><td>"  
+    + infoUser[i].descricao + "</td></tr>"  
+
+ }
+  
 }
